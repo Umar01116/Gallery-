0a6898d8be75668ad9071df1a8848b7e8a64c5a9
Umar01116 <uriaz3557@gmail.com>
2026-01-10 15:09:22 +0500

    remove btn and add use useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 const App = () => {
   const [Userdata, setUserdata] = useState([])
 
   const getdata = async ()=>{
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=15')
+    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=30')
     setUserdata(response.data)
   }
-  
+
+  useEffect(function(){
+    getdata()
+  })
+
   let printuserdata = "NO users available"
 
   if(Userdata.length > 0){
     printuserdata = Userdata.map(function(elem, idx){
-      return <div className='h-40 w-50 bg-white'>
-        <img className='h-full object-cover ' src={elem.download_url} alt="" />
+      return <div>
+        <a href={elem.url} target='_blank'>
+          <div className='h-40 w-50 overflow-hidden rounded-xl'>
+        <img className='h-full w-full object-cover' src={elem.download_url} alt="" />
       </div>
+      <h2 className='fomt-bold text-lg'>{elem.author}</h2>
+        </a>
+      </div> 
     })
   }
 
   return (
     <div className='bg-black overflow-auto h-screen text-white p-6'>
-      <button onClick={getdata} className='bg-blue-700 px-5 py-3 active:scale-95 rounded '>
-        Get data
-      </button>
       <div className='flex flex-wrap gap-4'>
         {printuserdata}
       </div>
